c6846c95cbf8195689aa133ca91527a95a559a4b
JorickPepin <48644518+JorickPepin@users.noreply.github.com>
2020-09-12 09:06:32 +0200

    customised destination file according to the player's name

--- src/recordsnbawiki/RecordsNBAWiki.java
+++ src/recordsnbawiki/RecordsNBAWiki.java
@@ -12,8 +12,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -28,13 +26,12 @@ public class RecordsNBAWiki {
      * @param args the command line arguments
      */
     public static void main(String[] args) {  
-        recuperationContenu(54148);
+        recuperationContenu(24275);
     }
 
     /**
      * Méthode permettant de récupérer le code source de la page
-     * 
-     * @param identifiant = l'identifiant du joueur
+     * @param identifiant = l'identifiant REALGM du joueur
      */
     private static void recuperationContenu(int identifiant) {
 
@@ -48,62 +45,89 @@ public class RecordsNBAWiki {
             // booleen permettant de garder que le contenu désiré
             boolean contenuValide = false;
             
-            String line; 
-            while ((line = br.readLine()) != null) {
+            String titre = "";
+            String ligne; 
+            while ((ligne = br.readLine()) != null) {
 
+                // récupération du titre de la page pour avoir le nom du joueur
+                if (ligne.startsWith("<title>")) {
+                    titre = ligne;
+                }
+                
                 // on détermine des "balises" pour récupérer que le contenu utile
-                if (line.equals("<h2>NBA Regular Season Career Highs</h2>")) {
+                if (ligne.equals("<h2>NBA Regular Season Career Highs</h2>")) {
                     contenuValide = true;
-                } else if (line.equals("<p class=\"footnote\">* NBA stats from 1946-1947</p><h2>NBA Career Comparison</h2>")) {
+                } else if (ligne.startsWith("<p class=\"footnote\">")) {
                     contenuValide = false;
                 }
                 
+                // si le contenu est compris entre les deux balises délimitées,
+                // on l'ajoute au SB
                 if (contenuValide) {
-                    sb.append(line);
+                    sb.append(ligne);
                     sb.append(System.lineSeparator());
-                    //ecritureDansFichier(line + "\n");
                 }     
-                
             }
-
-            // on récupère le contenu du SB (le code HTML utile) pour le transtyper en String
+            
+            // récupération du contenu du SB (le code HTML utile) pour le transtyper en String
             String html = sb.toString();
             
             Document document = Jsoup.parse(html);
                         
-            // on récupère le nombre d'éléments <td> (ceux qui nous intéressent)
+            // récupération du nombre d'éléments <td> (ceux qui nous intéressent)
             int size = document.select("td").size();
                     
             int i = 0;
             while (i < size) {
-                // on affiche les éléments <td>
+                // récupération des éléments <td>
                 Element link = document.select("td").get(i);
-                System.out.println("Text: " + link.text());
-                
+                String texteAEcrire = link.text() + "\n";
+                ecritureDansFichier(texteAEcrire, recuperationNomJoueur(titre));
                 i++;
             }
             
         } catch (MalformedURLException e) {
-            System.out.println(e);
-        } catch (IOException ex) {
-            Logger.getLogger(RecordsNBAWiki.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Erreur : " + e);
+        } catch (IOException e) {
+            System.err.println("Erreur : " + e);
         }
     }
 
     /**
      * Méthode permettant d'écrire le contenu dans un fichier .txt
-     * @param contenu = ce qu'on veut écrire
+     * @param contenu
      */
-    private static void ecritureDansFichier(String contenu) {
+    private static void ecritureDansFichier(String contenu, String nom) {
 
         byte data[] = contenu.getBytes();
         
-        Path fichier = Paths.get("test.txt");
+        // on crée ou écrit dans le fichier correspondant au nom du joueur
+        Path fichier = Paths.get("fichiers/" + nom + ".txt");
       
         try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(fichier, CREATE, APPEND))) {
             out.write(data, 0, data.length);
-        } catch (IOException x) {
-            System.err.println(x);
+        } catch (IOException e) {
+            System.err.println("Erreur : " + e);
         }
+    }
+    
+    /**
+     * Méthode permettant de récupérer le nom et le prénom du joueur à partir
+     * de la ligne <title> du code source
+     * @param titre = la ligne <title> du code source
+     * @return prénom_nom du joueur
+     */
+    private static String recuperationNomJoueur(String titre) {
+        
+        // récupération du titre sans la balise <title>
+        Document docTitre = Jsoup.parse(titre);
+        Element linkTitre = docTitre.select("title").first();
+        
+        String texte = linkTitre.text();
+        
+        String prenom = texte.split(" ")[0];
+        String nom = texte.split(" ")[1];
+            
+        return prenom + "_" + nom;
     }
 }
