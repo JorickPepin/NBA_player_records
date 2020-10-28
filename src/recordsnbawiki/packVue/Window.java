9990f7af7c8930f4fece551e5b3b78b2eeb57d20
JorickPepin <48644518+JorickPepin@users.noreply.github.com>
2020-10-28 21:13:03 +0100

    empty fields management

--- src/recordsnbawiki/packVue/Window.java
+++ src/recordsnbawiki/packVue/Window.java
@@ -1,6 +1,11 @@
 package recordsnbawiki.packVue;
 
+import java.awt.Color;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.border.Border;
 import recordsnbawiki.packLogic.Controller;
 import recordsnbawiki.packLogic.DataManagement;
 
@@ -30,8 +35,19 @@ public class Window extends JFrame implements Observer {
     @Override
     public void update(String code) {
         
+        switch (code) {
+            case "entriesAreInvalid":
+                label_alert.setText("Tous les champs ne sont pas remplis");
+                label_alert.setForeground(Color.RED);
+                label_alert.setHorizontalAlignment(JLabel.CENTER);
+                break;
+        }
     }
 
+    private void initTextFields() {
+        textField_RealGM.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        textField_ESPN.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+    }
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -43,13 +59,16 @@ public class Window extends JFrame implements Observer {
         button_submit = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         textArea_content = new javax.swing.JTextArea();
+        label_alert = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         panel1.setPreferredSize(new java.awt.Dimension(300, 122));
 
+        textField_RealGM.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         textField_RealGM.setPreferredSize(new java.awt.Dimension(250, 20));
 
+        textField_ESPN.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         textField_ESPN.setPreferredSize(new java.awt.Dimension(250, 20));
 
         label_RealGM.setText("Identifiant RealGM :");
@@ -102,6 +121,8 @@ public class Window extends JFrame implements Observer {
         textArea_content.setRows(5);
         jScrollPane1.setViewportView(textArea_content);
 
+        label_alert.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -109,11 +130,13 @@ public class Window extends JFrame implements Observer {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(100, 100, 100)
-                        .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
                         .addGap(50, 50, 50)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(100, 100, 100)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(label_alert, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(50, 50, 50))
         );
         layout.setVerticalGroup(
@@ -121,21 +144,48 @@ public class Window extends JFrame implements Observer {
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(26, 26, 26)
+                .addGap(6, 6, 6)
+                .addComponent(label_alert, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(32, Short.MAX_VALUE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void button_submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button_submitActionPerformed
-        int RealGM_id = Integer.parseInt(textField_ESPN.getText());
-        int ESPN_id = Integer.parseInt(textField_RealGM.getText());
+        initTextFields();
         
-        this.dataManagement = new DataManagement(RealGM_id, ESPN_id);
+        String RealGM_id = "";
+        String ESPN_id = "";
         
-        this.textArea_content.setText(dataManagement.getFinalContent());
+        boolean entriesAreValid = true;
+        
+        Border redBorder = BorderFactory.createLineBorder(Color.RED, 1);
+        
+        if (textField_RealGM.getText().length() > 0) {
+            RealGM_id = textField_RealGM.getText();
+        } else {
+            textField_RealGM.setBorder(redBorder);
+            entriesAreValid = false;
+        }
+       
+        if (textField_ESPN.getText().length() > 0) {
+            RealGM_id = textField_ESPN.getText();
+        } else {
+            textField_ESPN.setBorder(redBorder);
+            entriesAreValid = false;
+        }
+        
+        if (entriesAreValid) {
+            this.dataManagement = new DataManagement(Integer.parseInt(RealGM_id), Integer.parseInt(ESPN_id));
+        
+            this.textArea_content.setText(dataManagement.getFinalContent());
+        } else {
+            update("entriesAreInvalid");
+        }
+        
     }//GEN-LAST:event_button_submitActionPerformed
 
 
@@ -145,6 +195,7 @@ public class Window extends JFrame implements Observer {
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel label_ESPN;
     private javax.swing.JLabel label_RealGM;
+    private javax.swing.JLabel label_alert;
     private java.awt.Panel panel1;
     private javax.swing.JTextArea textArea_content;
     private javax.swing.JTextField textField_ESPN;
